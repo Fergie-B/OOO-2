4ea34688e218b3a9412dc634505c19b69f68e38a
Barry Ferguson <barry@revolvecreative.ie>
2022-12-15 18:44:26 +0000

    Added Page change function on Games rules page

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
 let mainUserPage = document.getElementById("user-login");
 let seeGameRules = document.getElementById("rules-screen");
 let showGameRules = document.getElementById("game-rules-btn");
+let hideGameRules = document.getElementById("close-btn");
 
 /**
  * Function to run the main page with user login
@@ -27,6 +28,12 @@ showGameRules.addEventListener("click", viewGameRules);
 
 function viewGameRules() {
     mainUserPage.style.display = "none";
-    seeGameRules.style.display = "block";
-    
+    seeGameRules.style.display = "block";   
+}
+
+hideGameRules.addEventListener("click", closeGameRules);
+
+function closeGameRules() {
+    mainUserPage.style.display = "block";
+    seeGameRules.style.display = "none";
 }
