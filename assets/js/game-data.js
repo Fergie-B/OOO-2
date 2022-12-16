a349ed5a5da0be8c1c23f1c36a398bd120aca2b9
Barry Ferguson <barry@revolvecreative.ie>
2022-12-16 17:53:51 +0000

    Added Image Array in Game-Data File

---
+++ assets/js/game-data.js
@@ -0,0 +1,58 @@
+// Image and Questions array to export to game.js
+let imageChoices = {
+    bond [
+        'assets/images/1-bond-question/daniel-craig.jpg',
+        'assets/images/1-bond-question/david-niven.jpg',
+        'assets/images/1-bond-question/george-lazenby.jpg',
+        'assets/images/1-bond-question/roger-moore.jpg',
+    ],
+
+    flags [
+        'assets/images/2-flags-question/finland.jpg',
+        'assets/images/2-flags-question/germany.jpg',
+        'assets/images/2-flags-question/iceland.jpg',
+        'assets/images/2-flags-question/morocco.jpg',
+    ],
+
+    brands [
+        'assets/images/3-brands-question/burger-king.jpg',
+        'assets/images/3-brands-question/facebook.jpg',
+        'assets/images/3-brands-question/instagram.jpg',
+        'assets/images/3-brands-question/twitter.jpg',
+    ],
+
+    cities [
+        'assets/images/4-cities-question/london.jpg',
+        'assets/images/4-cities-question/new-york.jpg',
+        'assets/images/4-cities-question/paris.jpg',
+        'assets/images/4-cities-question/sydney.jpg',
+    ],
+
+    actors [
+        'assets/images/5-actors-question/christian-bale.jpg',
+        'assets/images/5-actors-question/michael-keaton.jpg',
+        'assets/images/5-actors-question/robert-downey-jr.jpg',
+        'assets/images/5-actors-question/robert-pattinson.jpg',
+    ],
+
+    bond [
+        'assets/images/1-bond-question/daniel-craig.jpg',
+        'assets/images/1-bond-question/david-niven.jpg',
+        'assets/images/1-bond-question/george-lazenby.jpg',
+        'assets/images/1-bond-question/roger-moore.jpg',
+    ],
+
+    bond [
+        'assets/images/1-bond-question/daniel-craig.jpg',
+        'assets/images/1-bond-question/david-niven.jpg',
+        'assets/images/1-bond-question/george-lazenby.jpg',
+        'assets/images/1-bond-question/roger-moore.jpg',
+    ],
+
+    bond [
+        'assets/images/1-bond-question/daniel-craig.jpg',
+        'assets/images/1-bond-question/david-niven.jpg',
+        'assets/images/1-bond-question/george-lazenby.jpg',
+        'assets/images/1-bond-question/roger-moore.jpg',
+    ],
+}
