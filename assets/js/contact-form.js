3309050973f80bb24496d4cac777763bcf31f8dd
Barry Ferguson <barry@revolvecreative.ie>
2023-03-28 09:53:37 +0000

    Commiting to save progress

--- assets/js/contact-form.js
+++ assets/js/contact-form.js
@@ -1,5 +1,6 @@
 /**
  * Eventlistener function to monitor the clicking of the submit button
+ * Code from Email JS Tutorial at https://www.emailjs.com/docs/tutorial/creating-contact-form/
  */
 const buttonSubmit = document.getElementById("btn-submit");
 
@@ -11,8 +12,22 @@ document.getElementById("contact-form").addEventListener("submit", function (eve
         emailjs.sendForm('default_service', 'ooo-template', this)
                 .then(function() {
                     console.log('SUCCESS!');
+                 submittedMessage();   
+
                 }, function(error) {
                     console.log('FAILED...', error);
                 });
             });
-        
+
+/**
+ * Hides Contact Form and Display a message to the user that the form has submitted
+ */
+function submittedMessage() {
+    let html = `
+                <div class="contact-form>
+                    <p>Thank you, your message has been submitted</p>
+                    <button type="button"  class="btn btn-primary form-button" onclick="window.location.href='index.html';">Back to Game</button>
+                </div>
+                `;
+        document.getElementById("contact-form").innerHTML = html;
+}
